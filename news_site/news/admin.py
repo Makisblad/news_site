75278f17a88b1586a39488958a5471273ffa44b7
Makarova Anastasia <korzhak.an@gmail.com>
2022-09-18 15:59:49 +0300

    Admin part was cinfigured.The first template of index page was created.

--- news_site/news/admin.py
+++ news_site/news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class NewsAdmin (admin.ModelAdmin):
+    list_display = ('id','title', 'created_at', 'updated_at','is_published','categories')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'categories')
+
+class CategoryAdmin (admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- news_site/news/models.py
+++ news_site/news/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Название новости')
+    content = models.TextField(blank=True, verbose_name='Новость')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликована')
+    categories = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')# null=True для того, чтобы что-то было указано для уже заведенных записей
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', 'title']
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')# db_index - устанавливает индекс для удобного поиска
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
